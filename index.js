2de61a84873fb56d28f388cc0f3897905ebad8b6
krdlange <krdlange@gmail.com>
2023-02-24 23:47:52 +0100

    fixed slider functionality

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 function displayPrice(value) {
   let prices = [
-    "$8 per month",
-    "$12 per month",
-    "$16 per month",
-    "24 per month",
-    "36 per month",
+    "8",
+    "12",
+    "16",
+    "24",
+    "36",
   ];
+
+  let pageviews = ["10K pageviews","50K pageviews","100K pageviews","500k pageviews","1M pageviews"];
+  
+  if (value <= 1) {
+    document.getElementById("total-pageviews").innerHTML = pageviews[0];
+    document.getElementById("display-price").innerHTML = prices[0];
+  } else {
+    document.getElementById("total-pageviews").innerHTML = pageviews[value - 1];
+    document.getElementById("display-price").innerHTML = prices[value - 1];
+  }
 }
