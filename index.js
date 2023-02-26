e9987d2545bd78a78dbbcfbc8936cf87c88bc954
krdlange <krdlange@gmail.com>
2023-02-26 18:49:40 +0100

    added sass, compiled css and fixed slider ui

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 function displayPrice(value) {
-  let prices = [
-    "8",
-    "12",
-    "16",
-    "24",
-    "36",
+  let prices = ["$8.00", "$12.00", "$16.00", "$24.00", "$36.00"];
+
+  let pageviews = [
+    "10K pageviews",
+    "50K pageviews",
+    "100K pageviews",
+    "500k pageviews",
+    "1M pageviews",
   ];
 
-  let pageviews = ["10K pageviews","50K pageviews","100K pageviews","500k pageviews","1M pageviews"];
-  
   if (value <= 1) {
-    document.getElementById("total-pageviews").innerHTML = pageviews[0];
-    document.getElementById("display-price").innerHTML = prices[0];
+    document.getElementById("slider__title").innerHTML = pageviews[0];
+    document.getElementById("slider__amount").innerHTML = prices[0];
   } else {
-    document.getElementById("total-pageviews").innerHTML = pageviews[value - 1];
-    document.getElementById("display-price").innerHTML = prices[value - 1];
+    document.getElementById("slider__title").innerHTML = pageviews[value - 1];
+    document.getElementById("slider__amount").innerHTML = prices[value - 1];
   }
 }
